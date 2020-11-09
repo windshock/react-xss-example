70db678db63322bd53a7603991bdb5c6a4a2407c
windshock <windshock@gmail.com>
2020-11-09 13:14:36 +0900

    react-html-parser ANd dompurify 참조 페이지 추가

--- src/LinkExample.js
+++ src/LinkExample.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactHtmlParser from "react-html-parser";
 import { sanitize } from "dompurify";
+// react-html-parser ANd dompurify: https://pragmaticwebsecurity.com/articles/spasecurity/react-xss-part3.html
 
 export const LinkExample = () => {
-  const unsanitized =
-    '<a href="fb://profile/33138223345" onClick="alert("xss");" style="color:red;text-decoration:none;text-transform:uppercase;"><b>click me</b></a>';
+  const url = "fb://profile/33138223345";
+  const unsanitized = `<a href=${url} onClick="alert("xss");" style="color:red;text-decoration:none;text-transform:uppercase;"><b>click me</b></a>`;
   // onclick은 react-html-parse에서 보안 위협으로 간주하여 html로 파싱하지 않음
   // https://github.com/wrakky/react-html-parser/issues/8
   const sanitized = sanitize(unsanitized);
