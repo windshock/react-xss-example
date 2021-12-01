7f9d3a9f4b47c24a603994eb93e02394d62ba88b
windshock <windshock@gmail.com>
2021-12-01 12:33:27 +0900

    imoprt DOMPurify

--- src/LinkExampleInnerHTML.js
+++ src/LinkExampleInnerHTML.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 // 테스트 코드는 아래를 참고
 // https://github.com/cure53/DOMPurify/blob/307c7d0dde17a2d1f27049d6d39add24144ba87c/test/test-suite.js
 
